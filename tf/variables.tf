9a4f31712b5a9bda5ccd0fa12571d03d3dd7ca43
Mark Hall <mark.hall993@gmail.com>
2022-11-24 13:08:26 -0800

    Add Initial Terraform Infrastructure

---
+++ tf/variables.tf
@@ -0,0 +1,11 @@
+variable "app_name" {
+  description = "Main Title"
+  type        = string
+  default     = "markchall.com"
+}
+
+variable "region" {
+  description = "AWS Region"
+  type        = string
+  default     = "us-east-1"
+}
