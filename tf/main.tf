4e093b1586cef909ab8b82ed04004e2e804c040c
Mark Hall <mark.hall993@gmail.com>
2022-12-22 12:25:19 -0700

    Add remote backend.

--- tf/main.tf
+++ tf/main.tf
@@ -1,3 +1,13 @@
 provider "aws" {
   region = var.region
 }
+
+terraform {
+  backend "s3" {
+    bucket         = "lab-tf-state"
+    key            = "portfolio/terraform.tfstate"
+    region         = "us-east-2"
+    dynamodb_table = "terraform-state-locks"
+    encrypt        = true
+  }
+}
